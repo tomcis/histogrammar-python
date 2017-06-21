60fd8e514aa3a20ae6575ac03acb9eb02adc483e
Chetan Malhotra <chetanalwaysmalhotra@hotmail.com>
2017-06-22 01:35:03 +0530

    migrated from new-vega to vega for further development

---
+++ histogrammar/plot/vega/view.js
@@ -0,0 +1,6 @@
+var view = new vega.View(runtime)
+  .logLevel(vega.Warn) // set view logging level
+  .initialize(document.querySelector('#view')) // set parent DOM element
+  .renderer('canvas') // set render type (defaults to 'canvas')
+  .hover() // enable hover event processing
+  .run(); // update and render the view

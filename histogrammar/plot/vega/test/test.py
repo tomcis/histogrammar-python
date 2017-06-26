a56cdfaba6bf7f7f586f5d3bf8601841047dc12a
Chetan Malhotra <chetanalwaysmalhotra@hotmail.com>
2017-06-26 15:15:20 +0530

    some minor changes

--- histogrammar/plot/vega/test/test.py
+++ histogrammar/plot/vega/test/test.py
@@ -7,27 +7,23 @@ import random
 # The data of interest (value) is buried within some unweildy object (DataPoint)
 data = [random.random() for i in range(2000)]
 
-# add some "bad" values into the dataset
-data.extend([float('nan'), float('inf'), float("-inf"), 100, -100])
+
+
+
+# aggregation structure and fill rule
 h = hg.Bin(num=20, low=0, high=1, quantity=lambda x: x, value=hg.Count())
+
+# fill the histogram!
 for d in data:
     h.fill(d)
-
 ''''# histogrammar has kept track of these
 print ("NaN counter:       ", histogram.nanflow)
 print ("overflow counter:  ", histogram.overflow)
 print ("underflow counter: ", histogram.underflow)'''
 
 
-
-
-
-#test_file=open("test.json",'w+')
-
 #stores data in json format
-testdata = json.dumps(h.toJsonFile("test.json"))
-
+testdata = json.dumps(h.toJsonFile("test.json"), indent=4)
 print(testdata)
 
-#test_file.close()
 

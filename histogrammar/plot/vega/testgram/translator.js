71a51c025587892abd5a214c7c4de5fc083267ca
Chetan Malhotra <chetanalwaysmalhotra@hotmail.com>
2017-07-10 22:24:00 +0530

    var request added

--- histogrammar/plot/vega/testgram/translator.js
+++ histogrammar/plot/vega/testgram/translator.js
@@ -1,2 +1,3 @@
 //loading our json
 var requestURL = 'https://raw.githubusercontent.com/histogrammar/histogrammar-python/vega/histogrammar/plot/vega/test/test.json';
+var request = new XMLHttpRequest();

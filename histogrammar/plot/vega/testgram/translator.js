b52a5dcc50b19f2fb2c81c4e2f3dc55c7ef2b99e
Chetan Malhotra <chetanalwaysmalhotra@hotmail.com>
2017-07-11 17:24:41 +0530

    commit

--- histogrammar/plot/vega/testgram/translator.js
+++ histogrammar/plot/vega/testgram/translator.js
@@ -1,4 +1,8 @@
 $.getJSON("https://raw.githubusercontent.com/histogrammar/histogrammar-python/vega/histogrammar/plot/vega/test/test.json", function(json) {
     console.log(json);
+    for(var i=0;i<json.length;i++)
+    {
+        console.log(data.values[i]);
+    }
 });
 

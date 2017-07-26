24f8244a3a9634bb7c3b47fdce064a43b35c1436
Chetan Malhotra <chetanalwaysmalhotra@hotmail.com>
2017-07-26 22:40:39 +0530

    minor changes

--- histogrammar/plot/vega/server.py
+++ histogrammar/plot/vega/server.py
@@ -1,7 +1,8 @@
 #Initializing server configurations
 
+thefile='testgram/testgram.html'
 def app(environ, start_response):
-    content = (open('testgram/testgram.html').read())
+    content = (open(thefile).read())
     start_response('200 OK', [('Content-Type','text-html')])
     return [content]
 
@@ -10,12 +11,9 @@ def app(environ, start_response):
 if __name__=='__main__':
     try:
         from wsgiref.simple_server import make_server
-        httpd = make_server('0.0.0.0', 8080, app)
-        print("Serving at http://localhost:8080/")
+        port=8888
+        httpd = make_server('0.0.0.0', port, app)
+        print("Serving at http://localhost:"+str(port)+"/"+thefile)
         httpd.serve_forever()
     except KeyboardInterrupt:
         print('Goodbye!!')
-
-
-
-

783a4f68d0001f17380fff2d0973879fdcc68691
Chetan Malhotra <chetanalwaysmalhotra@hotmail.com>
2017-07-27 19:33:05 +0530

    minor changes

--- histogrammar/plot/vega/testgram/server.py
+++ histogrammar/plot/vega/testgram/server.py
@@ -1,6 +1,6 @@
 #Initializing server configurations
 
-testgram='testgram/testgram.html'
+testgram='testgram.html'
 
 def visualization(environ, start_response):
     vis_content = (open(testgram).read())
@@ -13,7 +13,7 @@ def visualization(environ, start_response):
 if __name__=='__main__':
     try:
         from wsgiref.simple_server import make_server
-        vis_port=9000
+        vis_port=8008
         vis_httpd = make_server('0.0.0.0', vis_port, visualization)
         print("Serving youre chart at http://localhost:"+str(vis_port)+"/"+testgram)
         vis_httpd.serve_forever()
